e3b111a3f79207676c0058b08dd7db70d3578f7a
DmitriX <dodge.241294@gmail.com>
2026-05-21 20:30:29 +0300

    Add wallet mapper

---
+++ wallet/src/main/java/com/dmitrix/wallet/application/WalletMapper.java
@@ -0,0 +1,36 @@
+package com.dmitrix.wallet.application;
+
+import com.dmitrix.wallet.api.model.WalletRequest;
+import com.dmitrix.wallet.api.model.WalletResponse;
+import com.dmitrix.wallet.domain.OperationType;
+import com.dmitrix.wallet.domain.commands.UpdateWalletCommand;
+import com.dmitrix.wallet.domain.entities.Wallet;
+import com.dmitrix.wallet.domain.queries.GetWalletQuery;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WalletMapper {
+
+    private OperationType toOperationType(WalletRequest.OperationTypeEnum operationType) {
+        return switch (operationType) {
+            case DEPOSIT -> OperationType.DEPOSIT;
+            case WITHDRAW -> OperationType.WITHDRAW;
+        };
+    }
+
+    public UpdateWalletCommand toCommand(WalletRequest request) {
+        return new UpdateWalletCommand(
+                request.getWalletId(),
+                request.getAmount(),
+                toOperationType(request.getOperationType())
+        );
+    }
+
+    public GetWalletQuery toQuery(WalletRequest request) {
+        return new GetWalletQuery(request.getWalletId());
+    }
+
+    public WalletResponse toResponse(Wallet wallet) {
+        return new WalletResponse().walletId(wallet.getWalletId()).balance(wallet.getBalance());
+    }
+}
